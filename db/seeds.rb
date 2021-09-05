930e486ad7f98f2b5920a13911a4c1cd8792afa8
maryamzain123 <smaryam.zain@gmail.com>
2021-09-05 13:59:45 -0500

    added table and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+a1 = Artist.create(name: "Brad Pitt")
+a2 = Artist.create(name: "Will Smith")
+a3 = Artist.create(name: "Cameron Diaz")
+
+
+m1 = Movie.new(title: "Fight Club")
+m1.artist = a1
+m1.save
+m2 = Movie.new(title: "Men in Black")
+m2.artist = a2
+m2.save
+m3 = Movie.new(title: "Bad Teacher")
+m3.artist = a3
+m3.save
+
+w1 = Watchlist.create(name: "Bucket List")
+w2 = Watchlist.create(name: "Popular Movies")
